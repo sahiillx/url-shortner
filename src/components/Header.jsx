bab676c60a304634f7c8c45eb216f5172ce5d0d5
Sahil Khan <sahilkhan0471@gmail.com>
2024-06-25 22:51:49 +0530

    updated Deshboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,58 +24,62 @@ const Header = () => {
 
   return (
     <>
-    <nav className="flex justify-between items-center py-4">
-      <Link to="/">
-        <img src="/logo.png" className="h-16" alt="Logo" />
-      </Link>
-      <div>
-        {!user ? (
-          <Button
-            onClick={() => {
-              navigate("/auth");
-            }}
-          >
-            Login
-          </Button>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
-              <Avatar>
-                <AvatarImage
-                  src={user?.user_metadata?.profile_pic}
-                  className="object-contain"
-                />
-                <AvatarFallback>SK</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>{user?.user_metadata?.name}</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <LinkIcon className="mr-2 h-4 w-4" />
-                <span>My Links</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="text-red-400">
-                <LogOut className="mr-2 h-4 w-4" />
-                <span
-                  onClick={() => {
-                    fnLogout().then(() => {
-                      fetchUser();
-                      navigate("/");
-                    });
-                  }}
-                >
-                  Logout
-                </span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-      
-    </nav>
-    
-    {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
+      <nav className="flex justify-between items-center py-4">
+        <Link to="/">
+          <img src="/logo.png" className="h-16" alt="Logo" />
+        </Link>
+        <div>
+          {!user ? (
+            <Button
+              onClick={() => {
+                navigate("/auth");
+              }}
+            >
+              Login
+            </Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+                <Avatar>
+                  <AvatarImage
+                    src={user?.user_metadata?.profile_pic}
+                    className="object-contain"
+                  />
+                  <AvatarFallback>SK</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  {user?.user_metadata?.name}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to="/dashboard" className="flex">
+                    
+                    <LinkIcon className="mr-2 h-4 w-4" />
+                    <span>My Links</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-red-400">
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span
+                    onClick={() => {
+                      fnLogout().then(() => {
+                        fetchUser();
+                        navigate("/");
+                      });
+                    }}
+                  >
+                    Logout
+                  </span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>
+
+      {loading && <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />}
     </>
   );
 };
